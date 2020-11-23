51fb63c08e1687627b71f93600393856ca44b2f0
lcxfs1991 <lcxfs1991@gmail.com>
2020-11-23 17:48:14 +0800

    feat: improve error msg

--- index.js
+++ index.js
@@ -295,7 +295,7 @@ const initCos = async () => {
           incrementalFiles.push(file);
         } else {
           console.log('err', result, result.data);
-          result.data && result.data.error && core.debug(result.data.error);
+          result.data && core.debug(result.data);
         }
       });
 
