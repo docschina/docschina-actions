eee9b4454afa8a4976981e53ef295db09964675a
lcxfs1991 <lcxfs1991@gmail.com>
2020-11-26 08:03:01 +0800

    feat: add back log

--- index.js
+++ index.js
@@ -294,8 +294,7 @@ const initCos = async () => {
 
           incrementalFiles.push(file);
         } else {
-          console.log('err: ', result, result.data);
-          // result.data && core.debug(result.data);
+          result.data && core.debug(result.data);
         }
       });
 
